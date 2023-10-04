18d69334c5c3141b14e55195ca6593de11363330
Wang <alexanderwang@ufl.edu>
2023-10-04 13:39:35 -0400

    Almost done with Week2

--- Week 2/main.js
+++ Week 2/main.js
@@ -1,6 +1,34 @@
 // DOM - Document object model
 
+// const parent = document.getElementById("main");
+
+// const createDivs = (parent, iter) => {
+//     const newDiv = document.createElement("div");
+//     newDiv.textContent = iter;
+//     newDiv.style.backgroundColor = "blue";
+//     newDiv.style.height = "200px";
+//     newDiv.style.width = "200px";
+//     newDiv.style.display = "flex";
+//     newDiv.style.margin = "10px"
+//     newDiv.style.justifyContent = "center";
+//     newDiv.style.alignItems = "center";
+//     parent.append(newDiv);
+// }
+
+// for (let x = 0; x < 10; x++) {
+//     createDivs(parent, x);
+// }
+
+// var x = parent.children[0];
+// console.log(x);
+// x.innerHTML = "hello";
+
+// parent.removeChild(parent.lastChild);
+
+// Javascript Event Listeners
 const parent = document.getElementById("main");
+const btn1 = document.getElementById("btn1");
+let index = 0;
 
 const createDivs = (parent, iter) => {
     const newDiv = document.createElement("div");
@@ -15,13 +43,7 @@ const createDivs = (parent, iter) => {
     parent.append(newDiv);
 }
 
-for (let x = 0; x < 10; x++) {
-    createDivs(parent, x);
-}
-
-var x = parent.children[0];
-console.log(x);
-x.innerHTML = "hello";
-
-parent.removeChild(parent.lastChild);
-
+btn1.addEventListener("click", btnClicked = () => {
+    createDivs(parent, index);
+    index++;
+});
